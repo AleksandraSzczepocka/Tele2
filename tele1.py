5f46b77cc6e5aade6763a05a2ab6e881b80108fa
AleksandraSzczepocka <251642@edu.p.lodz.pl>
2025-03-28 17:04:17 +0100

    Test

---
+++ tele1.py
@@ -0,0 +1,75 @@
+import numpy as np
+
+# Macierz Hamminga (taka sama jak w C++)
+matrix_H = np.array([
+    [1, 1, 1, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0],
+    [1, 1, 0, 0, 1, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
+    [1, 0, 1, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+    [0, 1, 0, 1, 0, 1, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0],
+    [1, 1, 1, 0, 1, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0],
+    [1, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0],
+    [0, 1, 1, 1, 1, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1, 0],
+    [1, 1, 1, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1]
+])
+
+BIT_NR = 8  # Liczba bitów wiadomości
+
+
+def encode(input_file, encoded_file):
+    with open(input_file, "rb") as infile, open(encoded_file, "w") as outfile:
+        byte = infile.read(1)
+        while byte:
+            byte_val = ord(byte)  # Konwersja bajtu na wartość liczbową
+            message = np.array([int(b) for b in format(byte_val, '08b')])  # Zamiana na listę bitów (numpy array)
+
+            # Poprawiona operacja: mnożymy wiadomość przez 8 pierwszych kolumn macierzy
+            check = np.dot(matrix_H[:, :BIT_NR], message) % 2
+
+            # Tworzymy zakodowany ciąg bitów
+            encoded_bits = np.concatenate((message, check))
+            outfile.write("".join(map(str, encoded_bits)) + "\n")
+
+            byte = infile.read(1)
+
+    print("Plik został zakodowany.")
+
+
+def decode(encoded_file, output_file):
+    with open(encoded_file, "r") as infile, open(output_file, "wb") as outfile:
+        for line in infile:
+            encoded_bits = [int(b) for b in line.strip()]
+            message = encoded_bits[:BIT_NR]
+            received_check = encoded_bits[BIT_NR:]
+
+            # Obliczanie błędów
+            mistakes = [(sum(encoded_bits * matrix_H[i]) % 2) for i in range(BIT_NR)]
+
+            if sum(mistakes) == 0:
+                pass  # Brak błędów
+            else:
+                # Znalezienie błędnego bitu
+                for i in range(16):
+                    if all(mistakes[j] == matrix_H[j][i] for j in range(BIT_NR)):
+                        encoded_bits[i] ^= 1  # Naprawa bitu
+                        break
+
+            # Konwersja naprawionej wiadomości na bajt
+            byte_val = int("".join(map(str, encoded_bits[:BIT_NR])), 2)
+            outfile.write(byte_val.to_bytes(1, 'big'))
+
+    print("Plik został zdekodowany.")
+
+
+# Menu wyboru
+if __name__ == "__main__":
+    print("1. Kodowanie pliku")
+    print("2. Dekodowanie pliku")
+    choice = input("Wybierz opcję: ")
+
+    if choice == "1":
+        file_name = input("Podaj nazwę pliku do zakodowania: ")
+        encode(file_name, "encoded.txt")
+    elif choice == "2":
+        decode("encoded.txt", "decoded.bin")
+    else:
+        print("Nieprawidłowy wybór.")
