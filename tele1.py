ba6cde2d85005a3282f1bed2e158f5152eee6e30
251642 Szczepocka Aleksandra <251642@edu.p.lodz.pl>
2025-03-29 13:17:42 +0100

    New version

--- tele1.py
+++ tele1.py
@@ -37,20 +37,35 @@ def decode(encoded_file, output_file):
     with open(encoded_file, "r") as infile, open(output_file, "wb") as outfile:
         for line in infile:
             encoded_bits = [int(b) for b in line.strip()]
-           # message = encoded_bits[:BIT_NR]
-           # received_check = encoded_bits[BIT_NR:]
+            message = encoded_bits[:BIT_NR]
+            received_check = encoded_bits[BIT_NR:]
 
-            # Obliczanie błędów
-            mistakes = [(sum(encoded_bits * matrix_H[i]) % 2) for i in range(BIT_NR)]
+            # Obliczanie błędów (syndrom)
+            mistakes = [sum(encoded_bits * matrix_H[i]) % 2 for i in range(BIT_NR)]
 
             if sum(mistakes) == 0:
                 pass  # Brak błędów
             else:
-                # Znalezienie błędnego bitu
+                # Sprawdzanie czy są dwa błędy (próbujemy wykryć dwie błędne kolumny)
+                error_found = False
                 for i in range(16):
-                    if all(mistakes[j] == matrix_H[j][i] for j in range(BIT_NR)):
-                        encoded_bits[i] ^= 1  # Naprawa bitu
+                    for j in range(i + 1, 16):
+                        # Sprawdzamy, czy kombinacja dwóch błędów może prowadzić do danego syndromu
+                        if all(mistakes[k] == (matrix_H[k][i] ^ matrix_H[k][j]) for k in range(BIT_NR)):
+                            # Naprawiamy oba błędy
+                            encoded_bits[i] ^= 1
+                            encoded_bits[j] ^= 1
+                            error_found = True
+                            break
+                    if error_found:
                         break
+
+                # Jeśli to pojedynczy błąd, naprawiamy tylko ten jeden błąd
+                if not error_found:
+                    for i in range(16):
+                        if all(mistakes[j] == matrix_H[j][i] for j in range(BIT_NR)):
+                            encoded_bits[i] ^= 1  # Naprawiamy pojedynczy błąd
+                            break
 
             # Konwersja naprawionej wiadomości na bajt
             byte_val = int("".join(map(str, encoded_bits[:BIT_NR])), 2)
