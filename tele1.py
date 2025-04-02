1cea953e9d2937d41c0572c6088ea88f17bc9b0f
251604 Pakos Dawid <251604@edu.p.lodz.pl>
2025-04-02 21:42:21 +0200

    Normal comments

--- tele1.py
+++ tele1.py
@@ -19,12 +19,12 @@ def encode(input_file, encoded_file):
         byte = infile.read(1)
         while byte:
             byte_val = ord(byte)  # Konwersja bajtu na wartość liczbową
-            message = np.array([int(b) for b in format(byte_val, '08b')])  # Zamiana na listę bitów (numpy array)
+            message = np.array([int(b) for b in format(byte_val, '08b')])  # Zamiana na listę bitów
 
-            # Poprawiona operacja: mnożymy wiadomość przez 8 pierwszych kolumn macierzy
+            # Mnożenie wiadomość przez 8 pierwszych kolumn macierzy
             check = np.dot(matrix_H[:, :BIT_NR], message) % 2
 
-            # Tworzymy zakodowany ciąg bitów
+            # Tworzenie zakodowanych ciągów bitów
             encoded_bits = np.concatenate((message, check))
             outfile.write("".join(map(str, encoded_bits)) + "\n")
 
@@ -37,8 +37,6 @@ def decode(encoded_file, output_file):
     with open(encoded_file, "r") as infile, open(output_file, "wb") as outfile:
         for line in infile:
             encoded_bits = [int(b) for b in line.strip()]
-            message = encoded_bits[:BIT_NR]
-            received_check = encoded_bits[BIT_NR:]
 
             # Obliczanie błędów (syndrom)
             mistakes = [sum(encoded_bits * matrix_H[i]) % 2 for i in range(BIT_NR)]
@@ -46,13 +44,13 @@ def decode(encoded_file, output_file):
             if sum(mistakes) == 0:
                 pass  # Brak błędów
             else:
-                # Sprawdzanie czy są dwa błędy (próbujemy wykryć dwie błędne kolumny)
+                # Dwa błędy
                 error_found = False
                 for i in range(16):
                     for j in range(i + 1, 16):
-                        # Sprawdzamy, czy kombinacja dwóch błędów może prowadzić do danego syndromu
+                        # Sprawdzanie, czy kombinacja dwóch kolumn może prowadzić do danego syndromu
                         if all(mistakes[k] == (matrix_H[k][i] ^ matrix_H[k][j]) for k in range(BIT_NR)):
-                            # Naprawiamy oba błędy
+                            # Naprawianie błędów
                             encoded_bits[i] ^= 1
                             encoded_bits[j] ^= 1
                             error_found = True
@@ -60,11 +58,11 @@ def decode(encoded_file, output_file):
                     if error_found:
                         break
 
-                # Jeśli to pojedynczy błąd, naprawiamy tylko ten jeden błąd
+                # Pojedynczy błąd
                 if not error_found:
                     for i in range(16):
                         if all(mistakes[j] == matrix_H[j][i] for j in range(BIT_NR)):
-                            encoded_bits[i] ^= 1  # Naprawiamy pojedynczy błąd
+                            encoded_bits[i] ^= 1  # Naprawianie błędu
                             break
 
             # Konwersja naprawionej wiadomości na bajt
